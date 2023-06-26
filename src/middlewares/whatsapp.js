fca58bbcf0336cf4a218c9be4d5108c54ec10b8f
Cristian Murua <cristianmurua1995@gmail.com>
2023-06-26 17:28:37 -0300

    twilio uninstalled

--- src/middlewares/whatsapp.js
+++ src/middlewares/whatsapp.js
@@ -1,15 +1,15 @@
-const accountSid = "AC896e45ae0e245623ab99527ee8edc64e";
-const authToken = "36df91aa07402569a19510d3be32536c";
-import twilio from "twilio";
-const client = twilio(accountSid, authToken);
+// const accountSid = "AC896e45ae0e245623ab99527ee8edc64e";
+// const authToken = "36df91aa07402569a19510d3be32536c";
+// import twilio from "twilio";
+// const client = twilio(accountSid, authToken);
 
-export function enviarAlertaWhatsApp(numeroTelefono, mensaje) {
-  client.messages
-    .create({
-      body: mensaje,
-      from: "+541173627750",
-      to: numeroTelefono,
-    })
-    .then((message) => console.log(message.sid))
-    .catch((error) => console.log(error));
-}
+// export function enviarAlertaWhatsApp(numeroTelefono, mensaje) {
+//   client.messages
+//     .create({
+//       body: mensaje,
+//       from: "+541173627750",
+//       to: numeroTelefono,
+//     })
+//     .then((message) => console.log(message.sid))
+//     .catch((error) => console.log(error));
+// }
