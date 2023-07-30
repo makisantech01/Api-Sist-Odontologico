a9f3c742a6d06a3a5aabe5dbf7e422095e71e711
Cristian Murua <cristianmurua1995@gmail.com>
2023-07-30 15:27:55 -0300

    mati odontograma y diente ok

--- src/routes/odontogramas.js
+++ src/routes/odontogramas.js
@@ -6,26 +6,14 @@ import {
   createOdontograma,
   updateOdontograma,
   deleteOdontograma,
-  // getAllDientes,
-  // getDienteById,
-  // createDiente,
-  // updateDiente,
-  // deleteDiente,
 } from "../controllers/odontograma.js";
 
 const router = Router();
 
 router.get("/", catchedAsync(getAllOdontogramas));
 router.get("/:id", catchedAsync(getOdontograma));
-router.post("/:id", catchedAsync(createOdontograma));
+router.post("/", catchedAsync(createOdontograma));
 router.put("/:id", catchedAsync(updateOdontograma));
 router.delete("/:id", catchedAsync(deleteOdontograma));
-
-// // Rutas para los dientes del odontograma
-// router.get("/dientes", catchedAsync(getAllDientes));
-// router.get("/dientes/:id", catchedAsync(getDienteById));
-// router.post("/dientes", catchedAsync(createDiente));
-// router.put("/dientes/:id", catchedAsync(updateDiente));
-// router.delete("/dientes/:id", catchedAsync(deleteDiente));
 
 export default router;
