0f97f9c8a3e6fbde51310750ade319f539ec3247
nicoflorentin <nicoflorentn@gmail.com>
2023-10-10 11:36:54 -0300

    add local and remote node scrips

--- src/config/config.cjs
+++ src/config/config.cjs
@@ -2,26 +2,34 @@ const Sequelize = require("sequelize")
 require("dotenv").config()
 const path = require("path")
 
-const {EXTERNAL_DB} = process.env
+const { EXTERNAL_DB, DB_HOST, DB_PASSWORD, DB_PORT, DB_USER, DB_NAME, NODE_ENV } = process.env
+
+console.log(NODE_ENV)
+
+let connectionConfig =
+	NODE_ENV === "local"
+		? `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`
+		: EXTERNAL_DB
 
 const sequelize = new Sequelize(
 	// process.env.DB_NAME,
 	// process.env.DB_USER,
 	// process.env.DB_PASSWORD,
-	EXTERNAL_DB,
 	// {
 	// 	host: process.env.DB_HOST,
 	// 	port: process.env.DB_PORT,
 	// 	dialect: "postgres",
 	// }
-  {
+	connectionConfig,
+	{
 		logging: false, // set to console.log to see the raw SQL queries
 		native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-		dialectOptions: {
-			ssl: {
-				require: true,
-			},
-		},
+		// dialectOptions: {
+		// 	ssl: {
+		// 		require: true,
+		// 	},
+		// },
+		dialect: "postgres",
 	}
 )
 
--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
-import app from "./app.js";
-import sequelize from "./config/config.cjs";
-import { config } from "dotenv";
-import "./models/index.js";
-import initializeData from "./utils/initData.js";
+import app from "./app.js"
+import sequelize from "./config/config.cjs"
+import { config } from "dotenv"
+import "./models/index.js"
+import initializeData from "./utils/initData.js"
 
-config();
+config()
 // initializeData();
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3001
 
 export async function main() {
-  try {
-    await sequelize.sync({ force: false });
-    console.log("DB Connection success!");
-    app.listen(port);
-    console.log(`Server listening on port ${port}`);
-  } catch (err) {
-    console.log(process.env.DB_NAME);
-    console.log("Unable to connect to the database", err);
-  }
+	try {
+		await sequelize.sync({ force: false, alter: true })
+		console.log("DB Connection success!")
+		app.listen(port)
+		console.log(`Server listening on port ${port}`)
+	} catch (err) {
+		console.log(process.env.DB_NAME)
+		console.log("Unable to connect to the database", err)
+	}
 }
 
-main();
+main()
